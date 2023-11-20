6805f7b2968eead9094808595657e7ec7777ec4d
Josep Vilaplana <josepvilamir@gmail.com>
2023-11-20 20:12:56 +0100

    Both Triangle vertex & fragment shaders created (witjout being linked)

--- Source/ModuleRenderExercise.cpp
+++ Source/ModuleRenderExercise.cpp
@@ -1,5 +1,6 @@
 #include "ModuleRenderExercise.h"
 #include <GL/glew.h>
+#include <iostream>
 
 ModuleRenderExercise::ModuleRenderExercise()
 {
@@ -22,6 +23,42 @@ bool ModuleRenderExercise::Init()
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    //Looks for any shader compile error
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+            "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); \n"
+        "}\0";
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+
 
 	return false;
 }
